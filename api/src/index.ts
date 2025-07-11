c7c993c1b37433a35636108ecd67ed831d8a1a7e
Delightsome Asolo <delightsomeasolo@gmail.com>
2025-07-11 23:43:39 +0100

    db

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,19 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
 
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 const port = 3000;
+
+// Connect to MongoDB Atlas
+mongoose
+  .connect(`${process.env.MONGODB_URI}`)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// console.log(process.env.MONGODB_URI);
 
 app.use("/", (req: Request, res: Response) => {
   res.send("On page");
